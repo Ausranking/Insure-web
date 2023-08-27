762b81c8515f4b0f992c2bfa8083b8395a6f4940
Austin Nze <austinnze1000@gmail.com>
2023-08-27 03:13:55 +0100

    soft touch

--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -1,9 +1,11 @@
 const BlogCard = ({ imgUrl, blogTitle, blog }) => {
   return (
     <>
-        <img src={imgUrl} className="h-20 w-20 bg-primary-blue rounded-full" />
+      <img src={imgUrl} className="h-20 w-20 bg-primary-blue rounded-full" />
       <h2 className="text-dark-violet text-2xl font-semibold ">{blogTitle}</h2>
-      <p className="text-xl text-primary-violet text-center ">{blog}</p>
+      <p className="text-xl px-12  text-primary-violet text-center tracking-tighter ">
+        {blog}
+      </p>
     </>
   );
 };
