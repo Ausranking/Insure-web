9a2842e3f2df3f109dd91aab2d33fe228ca345c1
Austin Nze <austinnze1000@gmail.com>
2023-08-29 13:45:11 +0100

    updates

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
-const Button = ({ text ,id }) => {
-  return <button className="ring-light-gray" id={id}>{text}</button>;
+const Button = ({ text, id }) => {
+  return (
+    <button className="ring-light-gray" id={id}>
+      {text}
+    </button>
+  );
 };
 
 export default Button;
